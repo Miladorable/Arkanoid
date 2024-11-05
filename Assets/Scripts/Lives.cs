ec2da97a403eb8cdb9319468b2f657d95ab41e39
Miladorable <Mila.Sales.Pro@gmail.com>
2024-11-05 12:31:41 +0100

    Code score

--- Assets/Scripts/Lives.cs
+++ Assets/Scripts/Lives.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms;
 using UnityEngine.UI;
 
 public class Lives : MonoBehaviour
 {
     public int lives = 3;
     public TMPro.TextMeshProUGUI LifeText;
+    public GameObject Lifecanva;
 
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -22,8 +24,9 @@ public class Lives : MonoBehaviour
         if (lives <= 0)
         {
             lives = 0;
-            SceneManager.LoadSceneAsync(1);
-        }
+            Lifecanva.SetActive(true);
+            Time.timeScale = 0;
+        }  
 
     }
     
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,9 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
- 
+    public int score = 0;
+    public TMPro.TextMeshProUGUI ScoreText;
+    public TMPro.TextMeshProUGUI ScoreTextFinal;
+    public GameObject canvas;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+            score += 1;
+            ScoreText.text = "Score : " + score;
+            ScoreTextFinal.text = ScoreText.text;
+        }
+
+    }
+
+    private void Update()
+    {
+        if(score==25)
+        {
+            canvas.SetActive(true);
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(0);
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
